298089ee7d8df11c9e581298340a6bfb54db9ecb
khushisharma2004 <ks969250@gmail.com>
2026-02-08 22:15:27 +0530

    Added localStorage and task features

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
+let tasks=[];
 const input = document.getElementById("TaskInput");
 const addBtn = document.getElementById("addBtn");
 const taskList = document.getElementById("taskList");
+if(localStorage.getItem("tasks")){
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+    tasks.forEach(task => addTaskToUI(task));
+}
 addBtn.addEventListener("click", function()  {
     if(input.value===""){
         alert("Please enter a Task");
         return;
     }
-    const li=document.createElement("li");
-    li.innerText=input.value;
+ tasks.push(input.value);   // array me add
+    saveTasks();               // localStorage me save
+    addTaskToUI(input.value);  // screen pe show
+
+    input.value = "";
+});
+
+function addTaskToUI(taskText){
+    const li = document.createElement("li");
+    li.innerText = taskText;
+   
      const delBtn = document.createElement("button");
     delBtn.innerText = "❌";
 
@@ -20,6 +34,12 @@ li.addEventListener("click", function () {
 delBtn.addEventListener("click", function (e) {
     e.stopPropagation
         li.remove();
+ tasks = tasks.filter(t => t !== taskText);
+        saveTasks();
     });
+}
 
-})
+/* STEP 5: Save to localStorage */
+function saveTasks(){
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
